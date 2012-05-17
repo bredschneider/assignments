c69d6dd93024f8ab1745570b4741ffad9591218c
jbred034 <bred0014@algonquinlive.com>
2012-05-17 11:11:59 -0400

    Assignment 2 for MTM1531.

---
+++ mtm1531/assignment-2/index.php
@@ -0,0 +1,69 @@
+<?php 
+$number1 = 0;
+$number2 = 0;
+$function = '+';
+$response = 0;
+
+if (isset($_POST['number1'])) {
+	$number1 = $_POST['number1'];
+}
+
+if (isset($_POST['number2'])) {
+	$number2 = $_POST['number2'];
+}
+
+if (isset($_POST['function'])) {
+	$function = $_POST['function'];
+}
+
+switch($function){
+	case '-':
+		$response = $number1 - $number2;
+		break;
+	case '*':
+		$response = $number1 * $number2;
+		break;
+	case '/':
+		$response = $number1 / $number2;
+		break;
+	case '+':
+	default:
+		$response = $number1 + $number2;
+		break;
+}
+
+$total = $response * 1.13;
+
+?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Money Calculator Form</title>
+	</head>
+	
+	<body>
+
+	<form method="post" action="index.php">
+		<label for="number1">Number 1</label>
+		<input type="number" id="number1" name="number1">
+		<label for="number2">Number 2</label>
+		<input type="number" id="number2" name="number2">
+		
+		<label for="function">Function</label>
+		<select if="function" name="function">
+			<option value="+">+</option>
+			<option value="-">-</option>
+			<option value="*">*</option>
+			<option value="/">/</option>
+		</select>
+		
+		<button>Calculate</button>
+		<strong>$<?php echo number_format($total, 2) ?></strong> 
+	</form>
+
+	</body>
+</html>
