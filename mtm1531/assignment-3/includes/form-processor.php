db9c7e75a052c2ef05dd5e6ce8a21c92d27c598f
jbred034 <bred0014@algonquinlive.com>
2012-05-24 11:46:37 -0400

    PHP for the form is working. No CSS added yet.

--- mtm1531/assignment-3/includes/form-processor.php
+++ mtm1531/assignment-3/includes/form-processor.php
@@ -2,7 +2,11 @@
 $error_msg = array();
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_EMAIL);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+$lang = filter_input(INPUT_POST, 'lang', FILTER_SANITIZE_NUMBER_INT);
+$notes = filter_input(INPUT_POST, 'notes', FILTER_SANITIZE_STRING);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if (empty($name)) {
@@ -11,5 +15,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		$error_msg ['email'] = true;
+	}
+	
+	if (mb_strlen($username) < 1 || mb_strlen($username) > 25) {
+		$error_msg ['username'] = true;
+	}
+	
+	if (empty($password)) {
+		$error_msg ['password'] = true;
+	}
+	
+	if (!array_key_exists($lang, $languages)) {
+		$errors['lang'] = true;
+	}
+	
+	if (!isset($_POST['terms'])) {
+		$errors['terms'] = true;
+	}
+	
+	if (empty($errors)) {
+		$headers = 'From: ' . $name . ' <' . $email . '>';
+		mail('jbred034@uottawa.ca', $languages[$lang], 'From Contact Form', $notes, $headers);
 	}
 }
--- mtm1531/assignment-3/index.php
+++ mtm1531/assignment-3/index.php
@@ -26,35 +26,39 @@ require_once 'includes/form-processor.php';
 			</div>
 			
 			<div>
-				<label for="email">Email Address<?php if(isset($error_msg['name'])) : ?><strong> is a required field.</strong><?php endif?></label>
+				<label for="email">Email Address<?php if(isset($error_msg['email'])) : ?><strong> is a required field.</strong><?php endif?></label>
 				<input type="email" id="email" name="email" required>
 			</div>
 			
 			<div>
-				<label for="username">Username</label>
+				<label for="username">Username<?php if(isset($error_msg['username'])) : ?><strong> is a required field.</strong><?php endif?></label>
 				<input id="username" name="username" required>
+                <p>Max length is 25 characters.</p>
 			</div>
 			
 			<div>
-				<label for="password">Password</label>
+				<label for="password">Password<?php if(isset($error_msg['password'])) : ?><strong> is a required field.</strong><?php endif?></label>
 				<input type="password" id="password" name="password" required>
 			</div>
 			
 			<div>
 				<fieldset>
 					<legend>Preferred Language</legend>
-					<input type="radio" id="languages">
+					<?php foreach($languages as $key => $value) : ?>
+                    	<input type="radio" id="languages" name="lang">
+           				<label value="<?php echo $key;?>"><?php echo $value;?></label>
+					<?php endforeach; ?>
 				</fieldset>
 			</div>
 			
 			<div>
 				<label for="notes">Notes</label>
-				<textarea id="notes" name="notes"></textarea>
+				<textarea id="notes" name="notes"><?php echo $notes;?></textarea>
 			</div>
 			
 			<div>
-				<label for="terms">Accept Terms</label>
 				<input type="checkbox" id="terms" value="1">
+				<label for="terms">Accept terms?<?php if(isset($errors['terms'])) : ?> <strong class="error">You must comply!</strong><?php endif;?></label>
 			</div>
 			
 			<button type="submit">Submit</button>
