1b2cf27604e64e008e1e5ee6108485c8c11cdbb3
jbred034 <bred0014@algonquinlive.com>
2012-05-28 15:03:38 -0400

    Updated index.html. Added content to general.js.

--- mtm1526/assignment-3/js/general.js
+++ mtm1526/assignment-3/js/general.js
@@ -1,5 +1,21 @@
-// JavaScript Document
-$(document).ready(function() {
-	var property = $('#property')
-	
+//jQuery
+$(document).ready(function ($) {
+var $circle = $('#circle')
+, $property = $('#property')
+, $color = $('#color')
+;
+
+$('form').on('submit', function (e) {
+var color = $color.val();
+
+e.preventDefault();
+
+if (color) {
+$circle.css($property.val(), color);
+}
+});
+
+$('#hide-show').on('click', function (e) {
+$circle.toggle(200);
+});
 });
