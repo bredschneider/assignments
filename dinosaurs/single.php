7b4a84079b8ca89326412b949e5df788b0fb63f6
jbred034 <bredd0014@algonquinlive.com>
2012-06-04 14:17:10 -0400

    Added single.php. Updated php and links in index.php.

---
+++ dinosaurs/single.php
@@ -0,0 +1,34 @@
+<?php 
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+//prepare() allows us to have variables in our sql
+//we can create placeholders and later fill them with some content
+//by using prepare we are protecting SQL injection attacks
+$sql=$db->prepare('
+SELECT id, dino_name, loves_meat, in_jurassic_park
+FROM dinosaurs
+WHERE id = :id;
+');
+//bindvalue(placeholder, variable, datatype)
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+$sql ->execute();
+$results = $sql->fetch();
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $results['dino_name']; ?></title>
+	</head>
+	
+	<body>
+		<h1><?php echo $results['dino_name']; ?></h1>
+		<dl>
+			<dt>Loves Meat</dt>
+			<dd><?php echo $results['loves_meat']; ?></dd>
+			<dt>In Jurassic Park</dt>
+			<dd><?php echo $results['in_jurassic_park']; ?></dd>
+		</dl>
+	</body>
+</html>
