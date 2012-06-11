b85befe45339d366256ddeac745f2169672c58d9
jbred034 <bred0014@algonquinlive.com>
2012-06-11 14:45:51 -0400

    Updated add.php

--- mtm1531/assignment-6/add.php
+++ mtm1531/assignment-6/add.php
@@ -1,10 +1,31 @@
+<?php 
+
+
+?>
+
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>Add a Movie</title>
+	</head>
+	
+	<body>
+		<form method="post" action="add.php">
+		<h1>Add a Movie</h1>
+			<div>
+				<label for="movie_title">Movie Title</label>
+				<input id="movie_title" name="movie_title">
+			</div>
+			<div>
+				<label for="release_date">Release Date</label>
+				<input id="release_date" name="release_date">
+			</div>
+			<div>
+				<label for="director">Director</label>
+				<input id="director" name="director">
+			</div>
+			
+		</form>
+	</body>
 </html>
