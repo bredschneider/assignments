63a18d28c0843e605f6e6dce8ceb7c2585770abc
jbred034 <bred0014@algonquinlive.com>
2012-06-11 13:21:37 -0400

    Initial push and commit of Assignment 5 files for Assignment 6

---
+++ mtm1531/assignment-6/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+require_once 'includes/db.php';
+$sql = $db->query('
+SELECT id, movie_title, release_date, director
+FROM movies
+ORDER BY movie_title ASC ');
+
+//var_dump($db->errorInfo());
+$results= $sql->fetchALL();
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Movies</title>
+		<link href="css/general.css" rel="stylesheet">
+	</head>
+	
+	<body>
+	<h1>Movies</h1>
+	<?php foreach($results as $movies):?>
+		<h2><a href="single.php?id=<?php echo $movies['id'];?>">
+		<?php echo $movies['movie_title']; ?>
+		</a>
+		</h2>
+		<dl>
+			<dt>Release Date</dt>
+			<dd><?php echo $movies['release_date']; ?></dd>
+			<dt>Director</dt>
+			<dd><?php echo $movies['director']; ?></dd>
+		</dl>
+	<?php endforeach; ?>
+	</body>
+</html>
